a41bf9bbcf7f4872b99a1ad6c34797a54f5d14fb
martin-anido <martosabe1@gmail.com>
2024-06-06 13:35:16 -0300

    DataPromocion

---
+++ include/DataPromocion.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#ifndef DATAPROMOCION
+#define DATAPROMOCION
+using namespace std;
+
+#include <string>
+#include "DTFecha.h"
+
+
+class DataPromocion{
+
+    private:
+        string nombre;
+        string descripcion;
+        DTFecha* fechaVenc;
+        float descuento;
+
+    public:
+        //Constructor
+        DataPromocion(string _nombre, string _descripcion, DTFecha* _fechaVenc, float _descuento);
+
+        //Destructor
+        ~DataPromocion();
+
+        //Getters
+        string getNombre();
+        string getDescripcion();
+        DTFecha* getFechaVenc();
+        float getDescuento();
+
+        //Setters
+        void setNombre(string _nombre);
+        void setDescripcion(string _descripcion);
+        void setFechaVenc(DTFecha* _fechaVenc);
+        void setDescuento(float _descuento);
+};
+
+#endif
---
+++ src/DataPromocion.cpp
@@ -0,0 +1,47 @@
+#include "../include/DataPromocion.h"
+
+DataPromocion::DataPromocion(string _nombre, string _descripcion, DTFecha* _fechaVenc, float _descuento){
+    nombre = _nombre;
+    descripcion = _descripcion;
+    fechaVenc = _fechaVenc;
+    descuento = _descuento;
+};
+
+DataPromocion::~DataPromocion(){};
+
+//Getters
+string DataPromocion::getNombre(){
+    return nombre;
+}
+
+string DataPromocion::getDescripcion(){
+    return descripcion;
+}
+
+DTFecha* DataPromocion::getFechaVenc(){
+    return fechaVenc;
+}
+
+float DataPromocion::getDescuento(){
+    return descuento;
+}
+
+//Setters
+void DataPromocion::setNombre(string _nombre){
+    nombre = _nombre;
+}
+
+void DataPromocion::setDescripcion(string _descripcion){
+    descripcion = _descripcion;
+}
+
+void DataPromocion::setFechaVenc(DTFecha* _fechaVenc){
+    fechaVenc = _fechaVenc;
+}
+
+void DataPromocion::setDescuento(float _descuento){
+    descuento = _descuento;
+}
+
+
+
