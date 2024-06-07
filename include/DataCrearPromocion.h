be986d9bd741ea31534bf64c818e76dd72170576
martin-anido <martosabe1@gmail.com>
2024-06-07 18:32:31 -0300

    descomentar cosas relacionadas a ParCodigoCantidad

--- include/DataCrearPromocion.h
+++ include/DataCrearPromocion.h
@@ -2,30 +2,31 @@
 #ifndef DATACREARPROMOCION
 #define DATACREARPROMOCION
 
+#include <set>
 #include "DataPromocion.h"
-//#include "ParCodigoCantidad.h"
+#include "ParCodigoCantidad.h"
 
 
 class DataCrearPromocion {
     private:
         string vendedor;
         DataPromocion infoP;
-        //set<ParCodigoCantida> prodCant;
+        set<ParCodigoCantidad> prodCant;
     public:
         //Constructor
-        DataCrearPromocion(string _vendedor, DataPromocion _infoP/*, set<ParCodigoCantidad> _prodCant*/);
+        DataCrearPromocion(string _vendedor, DataPromocion _infoP, set<ParCodigoCantidad> _prodCant);
         //Destructor
         ~DataCrearPromocion();
 
         //Getters   
         string getVendedor();
         DataPromocion getInfoP();
-        //set<ParCodigoCantidad> getProdCant();
+        set<ParCodigoCantidad> getProdCant();
 
         //Setters
         void setVendedor(string _vendedor);
         void setInfoP(DataPromocion _infoP);
-        //void setProdCant(set<ParCodigoCantidad> _prodCant);
+        void setProdCant(set<ParCodigoCantidad> _prodCant);
 
 };
 
