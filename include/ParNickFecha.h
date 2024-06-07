8f5fb933083e78f3cb21460bb45fc32e0a628b70
SimplementeDiego <diegodellepianefurrer@gmail.com>
2024-06-07 17:45:44 -0300

    ParNickFecha

---
+++ include/ParNickFecha.h
@@ -0,0 +1,21 @@
+#ifndef PARNICKFECHA
+#define PARNICKFECHA
+
+#include "DTFecha.h"
+#include <string>
+
+class ParNickFecha{
+
+    private:
+        string nickname;
+        DTFecha fecha;
+    public:
+        ParNickFecha(string _nickname, DTFecha _fecha);
+        ~ParNickFecha();
+
+        string getNickname();
+        DTFecha getFecha();
+
+};
+
+#endif
