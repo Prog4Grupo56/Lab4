86a9ffcfc6a99e865de8f541652c0c4bf4247047
PedroArsuaga <arsuagapedro@gmail.com>
2024-06-10 20:11:52 -0300

    Implementa CU crear promo, a medias

---
+++ src/CasosDeUso/CrearPromocion.cpp
@@ -0,0 +1,39 @@
+#include "../../include/CasosDeUso/CasosDeUso.h"
+
+#include "../../include/Clases/IUsuario.h"
+#include "../../include/Clases/Fabrica.h"
+
+
+void CrearPromocion()
+{
+    Fabrica* fabrica = Fabrica::getInstance();
+    IUsuario* interfazUsuario = fabrica -> getIUsuario();
+
+    //interfazUsuario -> ingresarDatosPromocion(data)
+    //Falta?
+
+
+    set<string> nicknamesVendedores = interfazUsuario -> obtenerListaNicknameVendedores();
+
+
+    //====Imprimir nicknames====
+    cout << "Seleccione un vendedor: \n";
+    string seleccion;
+    cin >> seleccion;
+    interfazUsuario -> seleccionarVendedor(seleccion); //Queda guardado en dataCrearP
+
+
+    //set<ParCodigoNombre> productos = IUsuario -> obtenerListaProductosVendedor(); //El include esta en el IUsuario.h
+    //====Imprimir productos===
+    cout << "Seleccione un producto:\n";
+    string id;
+    cin >> id;
+    cout << "Ingrese la cantidad minima:\n";
+    int cant;
+    cin >> cant;
+    interfazUsuario -> agregarProductoCantidad(ParCodigoCantidad(id,cant));
+
+
+    interfazUsuario -> confirmarAltaPromocion();
+
+};
