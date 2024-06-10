131514c2b371e7e5182e90305addf71806a09d14
PedroArsuaga <arsuagapedro@gmail.com>
2024-06-10 17:40:46 -0300

    Falto el cpp en el anterior

---
+++ src/Clases/Compra.cpp
@@ -0,0 +1,38 @@
+#include "../../include/Clases/Compra.h"
+
+
+
+Compra::Compra(Cliente* _cliente, float _montoFinal )
+{
+    cliente = _cliente;
+    montoFinal = _montoFinal;
+    contCompra = ControladorCompra::getInstancia();
+
+};
+Compra::~Compra()
+{
+};
+
+
+float Compra::getMontoFinal()
+{
+    return montoFinal;
+};
+DTFecha Compra::getFecha()
+{
+    return fecha;
+};
+set<CompraProducto*> Compra::getSetCompraProducto()
+{
+    return SetCompraProducto;
+}; //Este va?
+
+void Compra::setFecha(DTFecha f)
+{
+    fecha = f;
+};
+
+void Compra::agregarProducto(Producto* p, Compra* c)
+{
+    SetCompraProducto.insert(new CompraProducto(p,c));
+};
