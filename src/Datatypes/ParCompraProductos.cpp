bafc7df52d283282a4f137e2ee7fe20afb67af44
SimplementeDiego <diegodellepianefurrer@gmail.com>
2024-06-11 18:15:05 -0300

    Cambios

--- src/Datatypes/ParCompraProductos.cpp
+++ src/Datatypes/ParCompraProductos.cpp
@@ -1,7 +1,11 @@
 #include "../../include/Datatypes/ParCompraProductos.h"
 
+ParCompraProductos::ParCompraProductos(){
+
+}
+
 ParCompraProductos::ParCompraProductos(float _montoF, set<DataProducto> _dProductos){
-    fechaActual = DTFecha(0,0,0);
+    fechaActual = DTFecha(0,0,0); // ver tema de fecha actual
     dProductos = _dProductos;
     montoF = montoF;
 }
