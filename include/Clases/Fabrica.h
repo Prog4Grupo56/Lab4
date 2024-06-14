1e161bb665ac50154ce8075bc680d1b8f6cb908e
SimplementeDiego <diegodellepianefurrer@gmail.com>
2024-06-14 17:33:23 -0300

    Compila

--- include/Clases/Fabrica.h
+++ include/Clases/Fabrica.h
@@ -1,22 +1,27 @@
 #ifndef FABRICA_H
 #define FABRICA_H
 
+#include "IFecha.h"
 #include "IUsuario.h"
 #include "ICompra.h"
 #include "ControladorCompra.h"
 #include "ControladorUsuario.h"
+#include "ControladorFecha.h"
 
 class ControladorCompra;
+class ControladorUsuario;
 
 class Fabrica {
 private:
     Fabrica();
     IUsuario* instanciaContorladorUsuario;
     ICompra* instanciaControladorCompra;
+    IFecha* instanciaControladorFecha;
     static Fabrica* instancia;
 public:
     IUsuario* getIUsuario();
     ICompra* getICompra();
+    IFecha* getIFecha();
     static Fabrica* getInstance();
 };
 
